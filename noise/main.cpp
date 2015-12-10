3ac0e26d89f0a7dc5cfe3537d9c364eeb052d952
Damien Katz <me@damienkatz.com>
2015-12-10 15:56:25 -0800

    Initial Commit

---
+++ noise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  noise
+//
+//  Created by Damien Katz on 12/10/15.
+//  Copyright © 2015 Damien Katz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
