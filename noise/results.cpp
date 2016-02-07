31d29e6ef13ee8b073a67eda42726317ccabb4b5
Damien Katz <me@damienkatz.com>
2016-02-06 23:42:03 -0800

    got indexer compiling again.

--- noise/results.cpp
+++ noise/results.cpp
@@ -6,10 +6,36 @@
 //  Copyright © 2016 Damien Katz. All rights reserved.
 //
 
+#include <vector>
+#include <list>
+#include "query.hpp"
 #include "results.hpp"
+#include "query.hpp"
 
 
 namespace_Noise
+
+
+void DocResult::TruncateArrayPaths(size_t array_path_depth) {
+    for(auto ap : array_paths)
+        ap.resize(array_path_depth);
+}
+bool DocResult::IntersectArrayPaths(const DocResult& other) {
+    for (auto ap = array_paths.begin(); ap != array_paths.end(); ap++) {
+        bool found = false;
+        for (auto ap_other : array_paths) {
+            if (*ap == ap_other) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            auto del = ap++;
+            array_paths.remove(*del);
+        }
+    }
+    return array_paths.size() > 0;
+}
 
 
 namespace_Noise_end
