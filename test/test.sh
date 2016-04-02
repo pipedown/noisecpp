615f99d2942bf193e696fdd54e836a053b52c570
Doug Coleman <doug.coleman@gmail.com>
2016-04-01 17:00:37 -0700

    test.sh: Queries start on a line. Results have a space at the beginning of the line. For a file containing queries and results foo.txt, the test makes foo.txt-out.txt and diffs it against the original file. If the diff is empty, tests pass.

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 # ./test.sh ../build/index ../build/query
+# ./test.sh ../build/index ../build/query testdata.json testqueries.txt
 
-if [ $# -ne 2 ]; then
+if [ $# -eq 2 ]; then
+	DATA=../test/testdata.json
+	QUERIES=../test/testqueries.txt
+elif [ $# -eq 4 ]; then
+	DATA=$3
+	QUERIES=$4
+
+else
 	echo "Usage: $0 index-binary query-binary"
-    exit 1
+	echo "or"
+	echo "Usage: $0 index-binary query-binary data.json queries.txt"
+	exit
 fi
 
-$1 -cd testindex < ../test/testdata.json
-$2 testindex < ../test/testqueries.txt
+OUT=$QUERIES-out.txt
+$1 -cd testindex < $DATA
+$2 -t testindex < $QUERIES > $OUT
+
+DIFFOUT=$(diff $QUERIES $OUT)
+if [[ $DIFFOUT ]]; then
+	echo ==TEST FAILED==
+	echo $DIFFOUT
+fi
